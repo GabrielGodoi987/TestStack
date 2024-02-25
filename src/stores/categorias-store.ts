23b3ca2711ad081b570da6c3669f5208d342febd
Gabriel Godoi <110495432+GabrielGodoi987@users.noreply.github.com>
2024-02-25 01:04:52 -0300

    Adicionando ref em categorias, que recebe as listas de categorias

--- src/stores/categorias-store.ts
+++ src/stores/categorias-store.ts
@@ -2,24 +2,28 @@ import { defineStore } from 'pinia';
 import { Notify } from 'quasar';
 import { Categorias } from 'src/Models/categorias';
 import { api } from 'src/boot/axios';
+import { ref } from 'vue';
 
 export const useCategorias = defineStore('categorias-store', () => {
-  const categorias: Categorias[] = [];
+  const categorias = ref<Categorias[]>([]);
 
   function FindAll() {
     api
       .get('http://localhost:3000/categorias')
-      .then((res) => {
-        const data = res.data;
-        categorias.push(data);
+      .then(async (res) => {
+        const data = await res.data;
+        categorias.value = data;
         console.log(data);
+        Notify.create({
+          message: 'as categoria ta funfando meu nobre',
+          color: 'green',
+        });
       })
       .catch((err) => {
         Notify.create({
           message: 'erro ao buscar as categorias',
           color: 'red',
         });
-
         console.log(err);
       });
   }
